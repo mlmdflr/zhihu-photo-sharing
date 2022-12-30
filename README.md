f8f2462918a6c796714f57c8c7eb1a9ce14b9d35
mlmdflr <liao520ly@gmail.com>
2022-12-30 13:10:19 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # zhihu-photo-sharing
+
+# use
+https://greasyfork.org/zh-CN/scripts/457342-zhihu-photo-sharing
--- src/header.ts
+++ src/header.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            zhihu-photo-sharing
-// @namespace       https://github.com/mlmdflr/tampermonkey-ts-template
+// @namespace       https://github.com/mlmdflr/zhihu-photo-sharing
 // @version         0.0.1
 // @author          mlmdflr
 // @description     知乎分享不用在手动长截图了
